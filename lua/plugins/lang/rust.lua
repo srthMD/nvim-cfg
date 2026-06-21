c47ebd2e98b26cc6978d699327180ebf8e88bda9
srthMD <srthro@proton.me>
2026-06-21 13:35:53 +0300

    neverminde

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -1,11 +1,8 @@
 return {
   {
-    "vxpm/ferris.nvim",
-    ft = "rust",
-    opts = {
-      create_commands = true,
-      url_handler = "xdg-open",
-    },
+    "mrcjkb/rustaceanvim",
+    version = "^9",
+    lazy = false,
   },
 
   {
