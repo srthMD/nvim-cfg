f86e92ac40138ba36dfde5ad3bbb3450437ea99f
srthMD <srthro@proton.me>
2026-09-15 11:52:16 -0400

    add nasm support becuase i like doing it for funnies sometimes

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,25 +1,35 @@
 return {
   {
     "vyfor/cord.nvim",
-    build = ':Cord update',
+    build = ":Cord update",
     opts = {
       editor = {
         tooltip = vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch,
       },
 
       display = {
-        theme = "atom"
+        theme = "atom",
       },
     },
     enabled = false,
   },
 
   {
-    "brianhuster/unnest.nvim"
+    "brianhuster/unnest.nvim",
   },
 
-  { 
-    'nvim-mini/mini.nvim',
-    version = false 
+  {
+    "nvim-mini/mini.nvim",
+    version = false,
+  },
+
+  {
+    "Who5673/who5673-nasm", -- Or git@github.com:Who5673/who5673-nasm if you use SSH Key (recommended)
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/nvim-cmp",
+    },
+    ft = "nasm",
+    lazy = true,
   },
 }
